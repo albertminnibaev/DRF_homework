e54aac3ac0bb9e662c976d5c1b6f25769e7ba6f7
Albert <albert.minnibaeff@yandex.ru>
2023-12-01 21:23:13 +0400

    completed homework 27.2

--- config/settings.py
+++ config/settings.py
@@ -92,10 +92,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'course',
+        # 'USER': 'postgres',
+        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'course',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('PASSWORD_DATABASE')
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db'
     }
 }
 
@@ -188,7 +193,8 @@ CSRF_TRUSTED_ORIGINS = [
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+# CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://redis:6379/0'  # Этот вариант нужен при запуске docker
 
 # URL-адрес брокера результатов, также Redis
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
